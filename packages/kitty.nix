ab15374c86b41a65cc1a2d6713853487ba8da3bd
lucas <mendes.lucas9498@gmail.com>
2025-04-29 00:23:48 +0100

    feat: setup kitty config

--- packages/kitty.nix
+++ packages/kitty.nix
@@ -1,6 +1,19 @@
 { pkgs, ... }: {
 
    programs.kitty = {
-     enable = true;
+     enable = true;   
+     themeFile = "Molokai";
+
+     settings = {
+       background_opacity = 0.9;
+       background_blur = 22;
+     };
+
+     keybindings = {
+       "cmd+ctrl+t" = "new_tab_with_cwd";
+       "cmd+ctrl+w" = "close_other_tabs_in_os_window";
+       "cmd+l" = "previous_tab";
+       "cmd+r" = "next_tab";
+     }; 
    };
 }
